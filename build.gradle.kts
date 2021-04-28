0ef9db994a79646a54573580c71d8acec7a9368c
Giulia <giulia.brugnatti@studio.unibo.it>
2021-04-28 10:21:30 +0200

    Comments added

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,15 +14,15 @@ repositories {
 dependencies{
     implementation("junit:junit:4.13.1")
     implementation("commons-io:commons-io:+")
-    implementation("org.scala-lang:scala-library:2.12.2")
-    implementation("org.codehaus.groovy:groovy-all:2.4.15")
-    implementation(kotlin("script-runtime"))
+    implementation("org.scala-lang:scala-library:2.12.2")      //SCALA
+    implementation("org.codehaus.groovy:groovy-all:2.4.15")    //GROOVY
+    implementation(kotlin("script-runtime"))                            //KOTLIN
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
 
-application {
+application { //Without this, gladle build works,but gradle run not
     mainClass.set("Main")
 }
 
